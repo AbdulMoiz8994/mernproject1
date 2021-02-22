0f23694d2afc55fa0f507efee1b011ca460b27d4
AbdulMoiz8994 <moiza8994@gmail.com>
2021-02-22 20:06:29 +0500

    add these practice

--- src/App.js
+++ src/App.js
@@ -4,19 +4,10 @@ import './App.css';
 class App extends Component{
   nameFunction=() => 'Abdul moiz';
   render(){
-    // const name="Abdul moiz!"
-    const loading= false; 
-const showName=true
 
-  // if(loading){
-  //     return <h1>Loading...</h1>
-  // }
     return(
       <div>
-        {/* {loading ? <h1> Loading... </h1>: <h1>{this.nameFunction()}</h1>} */}
-        {loading ? <h1> Loading... </h1> : <h1>{showName && <h1>{this.nameFunction()}</h1>}</h1>}
-
-        {/* <h1>hello {name}</h1> */}
+        <h1>hello Moiz</h1>
       </div>
     )
   }
