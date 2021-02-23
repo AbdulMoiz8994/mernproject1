2959ab88249608ff88e29c26439ee46c3566d97c
AbdulMoiz8994 <moiza8994@gmail.com>
2021-02-23 23:31:04 +0500

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,41 +1,36 @@
-import React, { Component } from  'react'
-import './App.css';
-import NavBar from './Components/Header/NavBar';
-// import UserItem from './Components/Users/UserItem' 
-import Users from './Components/Users/Users'
-import axios from 'axios'
+import React, { Component } from "react";
+import "./App.css";
+import NavBar from "./Components/Header/NavBar";
+// import UserItem from './Components/Users/UserItem'
+import Users from "./Components/Users/Users";
+import axios from "axios";
 
-
-class App extends Component{
+class App extends Component {
   // nameFunction=() => 'Abdul moiz';
-  state={
+  state = {
     users: [],
-    laoding: false
+    laoding: false,
+  };
+  async componentDidMount() {
+    this.setState({ laoding: true });
+    try {
+      const { data } = await axios.get("https://api.github.com/users");
+      //  console.log(data)
+      this.setState({ users: data, laoding: false });
+    } catch (error) {
+      return error;
+    }
   }
-  async componentDidMount(){
-    this.setState({laoding: true})
-    try{
- const {data}= await axios.get('https://api.github.com/users') 
-//  console.log(data)
- this.setState({users: data,laoding: false})
-    }
-    catch(error){
-      return error
-    }
-   }
-  render(){
-
-    return(
-      <div className="container">
-   <NavBar title="List of GitHub Users By Abdul Moiz"/>
- {/* <UserItem/> */}
- <div className="container">
-     <Users laoding={this.state.laoding} users={this.state.users}/>
-     </div>
+  render() {
+    return (
+      <div className='container'>
+        <NavBar title='List of GitHub Users By Abdul Moiz' />
+        <div className='container'>
+          <Users laoding={this.state.laoding} users={this.state.users} />
+        </div>
       </div>
-    )
+    );
   }
 }
-
 
 export default App;
